831c1c478cf65db91f4df351038de0aa977f86b7
Rafael Oliveira <ra.pha@live.com>
2020-03-08 17:08:23 -0300

    Updates

---
+++ precache-manifest.55c834a4452d6ad61fdab60e0c587b25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8007cda06e51cbbe4e3113232f1ac69",
+    "url": "/material/index.html"
+  },
+  {
+    "revision": "0d2e2838ff3205d66978",
+    "url": "/material/static/css/main.3f41f457.chunk.css"
+  },
+  {
+    "revision": "795f455b39e424ad0c7d",
+    "url": "/material/static/js/2.6cbc086a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/material/static/js/2.6cbc086a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2e2838ff3205d66978",
+    "url": "/material/static/js/main.27f6e90c.chunk.js"
+  },
+  {
+    "revision": "c1beb574d9c61bc6ff19",
+    "url": "/material/static/js/runtime-main.e79ce8ba.js"
+  },
+  {
+    "revision": "bf30ef4dbdc4e1702f0d4bf58fe722ec",
+    "url": "/material/static/media/logo.bf30ef4d.png"
+  },
+  {
+    "revision": "00ce0872adf136db1a9f0b3f164103ca",
+    "url": "/material/static/media/permutacao_caotica_formula.00ce0872.png"
+  }
+]);
