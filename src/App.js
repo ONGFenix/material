9397f867df0350e3048c6fdb8960a4b10297337e
Rafael Oliveira <ra.pha@live.com>
2020-03-15 08:54:30 -0300

    Added password

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { HashRouter } from 'react-router-dom';
 
 import './style.css'
@@ -7,17 +7,68 @@ import Header from './components/header';
 import Sidebar from './components/sidebar';
 import Routes from './routes';
 
-const App = () => (
-  <div className="App">
-    <HashRouter basename="/material">
-      <Header />
-      <div className="middle">
-        <Sidebar />
-        <Routes />
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.validateLogin = this.validateLogin.bind(this);
+    this.state = {
+      isLoggedIn: false
+    }
+  }
+
+  validateLogin = () => {
+    let password = document.getElementById('senha');
+    if(password.value === 'ongfenix8576'){
+      console.log('test');
+      this.setState({isLoggedIn: true});
+    }else{
+      alert("Senha incorreta");
+    }
+    console.log(this.state);
+  }
+
+  Login = () =>(
+    <div className="login">
+        <h1>Insira a senha de acesso para o material</h1>
+        <form className="loginForm" onSubmit={this.validateLogin}>
+            <input type="password" id="senha" placeholder="Senha"/>
+            <input type="submit" value="Entrar"/>
+        </form>
+    </div>
+  )
+
+
+  Middle = () => {
+    if (this.state.isLoggedIn) {
+      return (
+        <div className="middle">
+          <Sidebar />
+          <Routes />
+        </div>
+      );
+    } else {
+      return (
+        <div className="middle">
+          {this.Login()}
+        </div>
+      );
+    }
+  }
+  
+  render(){
+    return(
+      <div className="App">
+        <HashRouter basename="/material">
+          <Header />
+            {this.Middle()}
+        </HashRouter>
       </div>
-    </HashRouter>
-  </div>
-);
+    )
+  }
+}
 
 
 export default App;
